931aabf7ec73b5b0aa5a2072e77b9ac8562c0174
qatrameez <mahmoudqatrameez@gmail.com>
2025-04-05 08:30:49 +0300

    initial commit

---
+++ folo.py
@@ -0,0 +1,15 @@
+usernameing = input("wellcome, enter your name please.")
+  
+ 
+if (usernameing) == ("ass") :
+    print("get the fuck out of here")
+    
+elif (usernameing) == ("ben"):
+      good_deeds = input("how many good deeds did you do this week ?")
+
+
+    
+if (good_deeds) == 31:
+       print("your wellcome")
+
+else: print ("get the fuck out of here")
